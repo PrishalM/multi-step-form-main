d1edb15e27f034de147f537bf0ed695602fa6944
Prishal <prishalmakwana@gmail.com>
2022-12-17 15:53:50 +0000

    Step1 page design done. Need to set up routing next

---
+++ multi-step-form/src/pages/Step4/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Step4 = () => {
+  return <h1>Step4</h1>;
+};
+
+export default Step4;
---
+++ multi-step-form/src/pages/Step5/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Step5 = () => {
+  return <h1>Step5</h1>;
+};
+
+export default Step5;
