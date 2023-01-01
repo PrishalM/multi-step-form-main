440a90e0acb36e69fcadcca3785f9c2a38f74df1
Prishal <prishalmakwana@gmail.com>
2023-01-01 14:03:20 +0000

    added _redirects file

--- multi-step-form/src/pages/NotFoundPage/index.js
+++ multi-step-form/src/pages/NotFoundPage/index.js
@@ -3,47 +3,9 @@ import React from "react";
 const NotFoundPage = () => {
   return (
     <>
-      <div className="mobileTopBar">
-        <div className="mobileTopBarContainer">
-          <span className="numberCircle active">1</span>
-          <span className="numberCircle">2</span>
-          <span className="numberCircle">3</span>
-          <span className="numberCircle">4</span>
-        </div>
-      </div>
+      <div className="mobileTopBar"></div>
       <div className="mainContainer">
-        <div className="desktopSideBar">
-          <div className="desktopSideBarContainer">
-            <div className="step1Container">
-              <span className="numberCircle active">1</span>
-              <div className="desktopSideBarTextContainer">
-                <p className="desktopSideBarHeader">step 1</p>
-                <p className="desktopSideBarText">your info</p>
-              </div>
-            </div>
-            <div className="step2Container">
-              <span className="numberCircle">2</span>
-              <div className="desktopSideBarTextContainer">
-                <p className="desktopSideBarHeader">step 2</p>
-                <p className="desktopSideBarText">select plan</p>
-              </div>
-            </div>
-            <div className="step3Container">
-              <span className="numberCircle">3</span>
-              <div className="desktopSideBarTextContainer">
-                <p className="desktopSideBarHeader">step 3</p>
-                <p className="desktopSideBarText">add-ons</p>
-              </div>
-            </div>
-            <div className="step4Container">
-              <span className="numberCircle">4</span>
-              <div className="desktopSideBarTextContainer">
-                <p className="desktopSideBarHeader">step 4</p>
-                <p className="desktopSideBarText">summary</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className="desktopSideBar"></div>
         <div className="mainContent">
           <div className="personalInfoContainer">
             <h1 className="stepHeader">Page Not Found</h1>
